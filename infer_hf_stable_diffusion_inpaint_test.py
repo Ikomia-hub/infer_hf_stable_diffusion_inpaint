494d6ef4230ecdc264554452cf3d202939a774f5
ambroise <ambroise.berthe@ikomia.com>
2023-08-03 17:32:40 +0200

    Fix test file

--- infer_hf_stable_diffusion_inpaint_test.py
+++ infer_hf_stable_diffusion_inpaint_test.py
@@ -8,9 +8,8 @@ logger = logging.getLogger(__name__)
 
 def test(t, data_dict):
     logger.info(f"===== Test::{t.name} =====")
-    # 1. Set task parameter if necessary. ex: task.set_parameters(t, {"iteration": 10, "size":3})
-    # 2. Get input data from data_dict. ex: img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
-    # 3. For each input to set
-    #   3.1 Get task input instance. ex: input_0 = t.getInput(0)
-    #   3.2 Set input data. ex: input_0.setImage(img)
+    t.set_parameters({"iteration": 2, "size":3})
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
+    input_0 = t.getInput(0)
+    input_0.setImage(img)
     return run_for_test(t)
